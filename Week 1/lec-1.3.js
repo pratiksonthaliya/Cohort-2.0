ff00bdb2866be980818f4307d9cf0f75433da42b
Pratik Sonthaliya <pratiksonthaliya@gmail.com>
2024-02-02 16:39:37 +0530

    learned about basic bash commands and nvm, npm, node

--- Week 1/lec-1.3.js
+++ Week 1/lec-1.3.js
@@ -5,21 +5,22 @@
 
 const { clearInterval } = require("timers");
 
+// // Assignment
 // 1. Create a counter in Javascript (counts down from 30 to 0)
-// let i = 3;
-// setInterval(() => {
-//   console.log(i);
-//   i--;
-//   if (i === 0) {
-//     clearInterval(int);
-//   }
-// }, 1000);
+let i = 30;
+setInterval(() => {
+  console.log(i);
+  i--;
+  if (i === 0) {
+    clearInterval(int);
+  }
+}, 1000);
 
 // 2. Calculate the time it takes between a setTimeout call and the inner function actually running
 const beforeTime = new Date().getSeconds();
 setTimeout(() => {
   console.log("hi there");
-  //can add afunction here
+  //can add a function here
   const afterTime = new Date().getSeconds();
   console.log(afterTime - beforeTime, "secs");
 }, 1465);
