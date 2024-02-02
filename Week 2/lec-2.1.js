0507212f3bb645fde5dab0c7a770612aefbf7120
Pratik Sonthaliya <pratiksonthaliya@gmail.com>
2024-02-02 23:43:10 +0530

    revised async await, promises

--- Week 2/lec-2.1.js
+++ Week 2/lec-2.1.js
@@ -1,4 +1,34 @@
 // revision of callbacks -> lec 1.4
 
+//// when to use async fns
+// do a network call
+// sleep/wait for some time
+// read a file
+
 // Q. If js is synchronus then how is it performing synchronous and asynchronous task simultanesouly ??
 // => actually it isnt performing asynchronous task, it is being deligated to web apis , c++ code of browsers/ os for fs
+
+// // Promises
+// normal async function
+function mySetTimeout(callback, duration) {
+  setTimeout(function () {
+    callback();
+  }, duration);
+}
+mySetTimeout(function () {}, 1000);
+
+// promisified async fn
+function promisifiedMySetTimeout(duration) {
+  const p = new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve();
+    }, duration);
+  });
+  return p;
+}
+
+const ans = promisifiedMySetTimeout(2000);
+console.log(ans);
+ans.then(function () {
+  console.log("timeout is done");
+});
