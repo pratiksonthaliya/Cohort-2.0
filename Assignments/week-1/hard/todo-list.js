99bb79fc8df512fc703d3ad0274d2e244586924f
Pratik Sonthaliya <pratiksonthaliya@gmail.com>
2024-02-05 08:04:29 +0530

    added solution of assignment 1 hard -> todo-list

--- Assignments/week-1/hard/todo-list.js
+++ Assignments/week-1/hard/todo-list.js
@@ -11,7 +11,39 @@
 */
 
 class Todo {
-
+  constructor() {
+    this.todoList = [];
+  }
+  add(todo) {
+    this.todoList.push(todo);
+  }
+  remove(indexOfTodo) {
+    let newTodo = [];
+    for (let i = 0; i < this.todoList.length; i++) {
+      if (i !== indexOfTodo) {
+        newTodo.push(this.todoList[i]);
+      }
+    }
+    this.todoList = newTodo;
+  }
+  update(index, updatedTodo) {
+    for (let i = 0; i < this.todoList.length; i++) {
+      if (i === index) {
+        this.todoList[i] = updatedTodo;
+      }
+    }
+  }
+  getAll() {
+    return this.todoList;
+  }
+  get(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todoList.length) {
+      return this.todoList[indexOfTodo];
+    } else return null;
+  }
+  clear() {
+    this.todoList = [];
+  }
 }
 
 module.exports = Todo;
